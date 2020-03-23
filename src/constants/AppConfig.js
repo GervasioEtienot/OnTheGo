61d96767aa03671004b29a54837aad3f19513f38
GervasioEtienot <gervasioe@gmail.com>
2020-03-23 19:05:45 -0300

    Se agregó la firma y la versión <AppConfig>.

--- src/constants/AppConfig.js
+++ src/constants/AppConfig.js
@@ -20,7 +20,7 @@ const AppConfig = {
       icon: 'es',
    },
    enableUserTour: false, // process.env.NODE_ENV === 'production' ? true : false,  // Enable / Disable User Tour
-   copyRightText: 'Reactify © 2019 All Rights Reserved.',      // Copy Right Text
+   copyRightText: 'QuanTech v0.0.2 © 2020 All Rights Reserved.',      // Copy Right Text
    // light theme colors
    themeColors: {
       'primary': '#5D92F4',
